b8e102fec6a6584ca3625e489a2a7e628ca7e2ec
Lucas Furlong <trollinc.5@gmail.com>
2025-04-18 11:38:17 -0400

    update firebase hosting configuration and improve project component structure

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,9 @@
-import { Component, ElementRef, ViewChild, ChangeDetectorRef } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { gsap } from 'gsap';
 import { CommonModule } from '@angular/common';
 
@@ -7,54 +12,73 @@ import { CommonModule } from '@angular/common';
   selector: 'app-projects',
   imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent {
-
   constructor(private cdr: ChangeDetectorRef) {}
 
   @ViewChild('modalCard') modal!: ElementRef;
   @ViewChild('modalBackdrop') backdrop!: ElementRef;
-  activeIndex = 0
+  activeIndex = 0;
   private flashTween: gsap.core.Tween | null = null;
   selectedProject: any = null;
   projects = [
     {
       title: 'chromatica',
       image: '/assets/projects/chromatica.jpg',
-      description: 'AI-driven music generation mobile app using Angular, Ionic, Supabase, and TensorFlow.js. recently depricated due to Spotify API changes.',
-      links: [ { url: 'https://github.com/lucas-codes-stuff/chromatica', label: 'view project' } ]
+      description:
+        'AI-driven music generation mobile app using Angular, Ionic, Supabase, and TensorFlow.js. recently depricated due to Spotify API changes.',
+      links: [
+        {
+          url: 'https://github.com/lucas-codes-stuff/chromatica',
+          label: 'view project',
+        },
+      ],
     },
     {
       title: 'drisca',
       image: '/assets/projects/drisca.jpg',
-      description: 'mobile app focused on helping users discover their limitations and improve daily habits through daily prompts and posts after completion. made with React Native, Expo, and Supabase.',
-      links: [ { url: '', label: 'in progress! (repo is private)' } ]
+      description:
+        'mobile app focused on helping users discover their limitations and improve daily habits through daily prompts and posts after completion. made with React Native, Expo, and Supabase.',
+      links: [{ url: '', label: 'in progress! (repo is private)' }],
     },
     {
       title: 'friend matching app',
       image: '/assets/projects/social-app.png',
-      description: 'Live crypto dashboard using real-time APIs.',
-      links: [ { url: '', label: 'in progress! (repo is private)' } ]
+      description:
+        'friend matching application thats similar to tinder or other swiping apps. purely platonic. purely focused on good connections. LGBTQIA+ friendly and specificly inclusive. made with React Native, Expo, and Supabase.',
+      links: [{ url: '', label: 'in progress! (repo is private)' }],
     },
     {
       title: 'du file system',
       image: '/assets/projects/dufs.png',
-      description: 'a file system (terminal based) built with C# and .NET Core, using a custom file system driver and a custom file system API.',
-      links: [ { url: 'https://github.com/lucas-codes-stuff/du-file-system', label: 'view project' } ]
+      description:
+        'a file system (terminal based) built with C# and .NET Core, using a custom file system driver and a custom file system API.',
+      links: [
+        {
+          url: 'https://github.com/lucas-codes-stuff/du-file-system',
+          label: 'view project',
+        },
+      ],
     },
     {
       title: 'dog and cat classifier',
       image: '/assets/projects/dog-cat.png',
-      description: 'small React and FastAPI AI project to classify images of dogs and cats.',
-      links: [ { url: 'https://github.com/lucas-codes-stuff/dog-cat-classify', label: "view project" } ]
+      description:
+        'small React and FastAPI AI project to classify images of dogs and cats.',
+      links: [
+        {
+          url: 'https://github.com/lucas-codes-stuff/dog-cat-classify',
+          label: 'view project',
+        },
+      ],
     },
-  ];  
+  ];
 
   getCardClass(index: number) {
     const offset = index - this.activeIndex;
     const depth = Math.abs(offset);
-  
+
     return {
       'blur-sm': depth > 0,
       'scale-100': offset === 0,
@@ -67,15 +91,15 @@ export class ProjectsComponent {
       'translate-x-[200px]': offset === 1,
     };
   }
-  
+
   getZIndex(index: number) {
     return 100 - Math.abs(index - this.activeIndex);
   }
-  
+
   next() {
     if (this.activeIndex < this.projects.length - 1) this.activeIndex++;
   }
-  
+
   prev() {
     if (this.activeIndex > 0) this.activeIndex--;
   }
@@ -90,14 +114,14 @@ export class ProjectsComponent {
     // Define a max rotation (in degrees)
     const maxRotation = 10;
     const rotateY = -offsetX * maxRotation; // horizontal mouse affects vertical rotation
-    const rotateX = offsetY * maxRotation;  // vertical mouse affects horizontal rotation
+    const rotateX = offsetY * maxRotation; // vertical mouse affects horizontal rotation
 
     gsap.to(card, {
       rotationX: rotateX,
       rotationY: rotateY,
       transformPerspective: 500, // adds depth
       duration: 0.3,
-      ease: 'power2.out'
+      ease: 'power2.out',
     });
   }
 
@@ -107,7 +131,7 @@ export class ProjectsComponent {
       rotationX: 0,
       rotationY: 0,
       duration: 0.5,
-      ease: 'power2.out'
+      ease: 'power2.out',
     });
   }
 
@@ -115,37 +139,37 @@ export class ProjectsComponent {
     gsap.to(arrow, {
       scale: 1.2,
       duration: 0.3,
-      ease: 'power2.out'
+      ease: 'power2.out',
     });
-  
+
     this.flashTween = gsap.to(arrow, {
       opacity: 0.5,
       duration: 0.5,
       ease: 'power2.inOut',
       repeat: -1,
-      yoyo: true
+      yoyo: true,
     });
   }
-  
+
   onArrowLeave(arrow: HTMLElement) {
     gsap.to(arrow, {
       scale: 1,
       duration: 0.3,
-      ease: 'power2.out'
+      ease: 'power2.out',
     });
-  
+
     // Kill the flashing animation
     this.flashTween?.kill();
     this.flashTween = null;
-  
+
     // Reset opacity
     gsap.to(arrow, {
       opacity: 1,
       duration: 0.2,
-      ease: 'power2.out'
+      ease: 'power2.out',
     });
   }
-  
+
   openModal(project: any) {
     this.selectedProject = project;
     this.cdr.detectChanges(); // Ensure the modal is updated with the selected project
@@ -165,7 +189,7 @@ export class ProjectsComponent {
       }
     });
   }
-  
+
   closeModal() {
     // Animate the modal closing
     if (this.modal) {
@@ -175,11 +199,11 @@ export class ProjectsComponent {
         duration: 0.3,
         onComplete: () => {
           this.selectedProject = null; // Reset after animation
-        }
+        },
       });
       gsap.to(this.backdrop.nativeElement, {
         opacity: 0,
-        duration: 0.3
+        duration: 0.3,
       });
     }
   }
